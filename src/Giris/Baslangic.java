499003f9764f964da8811d9a01c3c6a5008bb6c5
Gorkem <grkmgry@gmail.com>
2021-12-27 23:18:23 +0300

    Proje oluşturuldu

---
+++ src/Giris/Baslangic.java
@@ -0,0 +1,13 @@
+package Giris;
+import java.util.Scanner;
+
+public class Baslangic {
+    public static void main(String[] args) {
+        int a;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Giriş Yapınız:");
+        a= input.nextInt();
+        System.out.println("Girdiğiniz veri : " + a);
+    }
+}
